cc2c7c959da53c3961c067371af851b874b9cb39
tosyaliselim <selimtosyali@protonmail.com>
2021-02-28 22:06:28 +0300

    Staircase Problem

---
+++ 1-StaircaseProblem/Solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public static int solve(int n, int[] arr) {
+
+        if(n==0) return 1;
+        if(n<0) return 0;
+        else {
+            int t = 0;
+            for(int i: arr){
+                t += solve(n - i, arr);
+            }
+            return t;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solve(6, new int[]{1, 2, 3}));
+    }
+
+}
